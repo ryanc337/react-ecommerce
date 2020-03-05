b1cb1f87bd27629aeaa631f13e76de65b78db017
ryanc337 <cordingley.ryan@gmail.com>
2020-03-04 16:16:34 -0800

    styled menu items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './pages/homepage/homepage.component';
+import './App.css';
 
 function App() {
   return (
