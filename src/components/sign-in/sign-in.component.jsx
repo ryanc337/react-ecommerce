aecb8676185949167680db87d93dd072703bd506
ryanc337 <cordingley.ryan@gmail.com>
2020-03-08 17:44:25 -0700

    added signin page and component

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  render() {
+    return(
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign In with your email and password</span>
+
+        <form>
+          <input name='email' type='email' value={this.state.email} required />
+          <label>Email</label>
+          <input name='password' type='password' value={this.state.password} required />
+          <label>Password</label>
+        </form>
+      </div>
+    )
+  }
+};
+
+export default SignIn;
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUpPage = () => {
+  return(
+    <div classNam='sign-in-and-sign-up-page'>
+      Sign IN
+    </div>
+  )
+};
+
+export default SignInAndSignUpPage;
