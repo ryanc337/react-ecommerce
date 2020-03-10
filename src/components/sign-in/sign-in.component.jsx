0b06da0a654f93f65ff9cf696c5b01c39276a37d
ryanc337 <cordingley.ryan@gmail.com>
2020-03-09 18:05:52 -0700

    created button component and styled in for logins

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -47,7 +48,7 @@ class SignIn extends React.Component {
           handleChange={this.handleChange}
           required />
 
-          <input type='submit' value='Submit Form'/>
+          <CustomButton type='submit'>Sign In</CustomButton>
         </form>
       </div>
     )
