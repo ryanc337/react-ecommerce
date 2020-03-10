2a0a51b761b09961f44fb0b77d4614edb637adff
ryanc337 <cordingley.ryan@gmail.com>
2020-03-09 17:28:49 -0700

    added basic jsx for signin page

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -11,17 +11,41 @@ class SignIn extends React.Component {
     }
   }
 
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.setState({ email: '', password: ''});
+  }
+
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  }
+
   render() {
     return(
       <div className='sign-in'>
         <h2>I already have an account</h2>
         <span>Sign In with your email and password</span>
 
-        <form>
-          <input name='email' type='email' value={this.state.email} required />
+        <form onSubmit={this.handleSubmit}>
+          <input 
+          name='email' 
+          type='email' 
+          value={this.state.email}
+          onChange={this.handleChange} 
+          required />
           <label>Email</label>
-          <input name='password' type='password' value={this.state.password} required />
+          <input 
+          name='password' 
+          type='password' 
+          value={this.state.password} 
+          onChange={this.handleChange}
+          required />
           <label>Password</label>
+
+          <input type='submit' value='Submit Form'/>
         </form>
       </div>
     )
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component';
 import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUpPage = () => {
   return(
     <div classNam='sign-in-and-sign-up-page'>
-      Sign IN
+      <SignIn />
     </div>
   )
 };
